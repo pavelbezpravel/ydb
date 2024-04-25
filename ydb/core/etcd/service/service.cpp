52f7c63d69d72ed7b532198a87bce28a0257e7c3
pavelbezpravel <p.talashchenko@gmail.com>
2024-04-26 00:31:42 +0300

    maintenance: cleanup

--- ydb/core/etcd/service/service.cpp
+++ ydb/core/etcd/service/service.cpp
@@ -12,11 +12,11 @@
 #include <ydb/library/actors/core/actor_bootstrapped.h>
 #include <ydb/library/actors/core/hfunc.h>
 #include <ydb/library/actors/core/log.h>
+
 #include <ydb/library/table_creator/table_creator.h>
 #include "ydb/library/services/services.pb.h"
 
-#define LOG_E(stream) LOG_ERROR_S(*NActors::TlsActivationContext, NKikimrServices::KQP_PROXY, "[ydb] [EtcdService]: " << stream)
-#define LOG_D(stream) LOG_DEBUG_S(*NActors::TlsActivationContext, NKikimrServices::KQP_PROXY, "[ydb] [EtcdService]: " << stream)
+#define LOG_E(stream) LOG_ERROR_S(*NActors::TlsActivationContext, NKikimrServices::KQP_PROXY, "[ydb] [ETCD_FEATURE] [EtcdService]: " << stream)
 
 namespace NYdb::NEtcd {
 
@@ -54,30 +54,22 @@ private:
     }
 
     void CreateRevisionTable() {
-        std::cerr << "TEvEtcdRevision::TEvCreateTableRequest\n";
         Register(NYdb::NEtcd::CreateRevisionTableCreateActor(NKikimrServices::KQP_PROXY, {}, Path, Cookie++));
     }
 
-    void Handle(TEvEtcdRevision::TEvCreateTableResponse::TPtr& ev) {
-        std::cerr << "TEvEtcdRevision::TEvCreateTableResponse\n";
-        LOG_D(ev->ToString());
+    void Handle(TEvEtcdRevision::TEvCreateTableResponse::TPtr&) {
         Register(NYdb::NEtcd::CreateRevisionTableInitActor(NKikimrServices::KQP_PROXY, {}, Path, Cookie++));
     }
 
-    void Handle(TEvEtcdRevision::TEvRevisionResponse::TPtr& ev) {
-        std::cerr << "TEvEtcdRevision::TEvRevisionResponse\n";
-        LOG_D(ev->ToString());
+    void Handle(TEvEtcdRevision::TEvRevisionResponse::TPtr&) {
         ProcessCached();
     }
 
     void CreateKVTable() {
-        std::cerr << "TEvEtcdKV::TEvCreateTableRequest\n";
         Register(NYdb::NEtcd::CreateKVTableCreateActor(NKikimrServices::KQP_PROXY, {}, Path, Cookie++));
     }
 
-    void Handle(TEvEtcdKV::TEvCreateTableResponse::TPtr& ev) {
-        std::cerr << "TEvEtcdKV::TEvCreateTableResponse\n";
-        LOG_D(ev->ToString());
+    void Handle(TEvEtcdKV::TEvCreateTableResponse::TPtr&) {
         ProcessCached();
     }
 
@@ -95,7 +87,6 @@ private:
     }
 
     void Handle(TEvEtcdKV::TEvRangeRequest::TPtr& ev) {
-        std::cerr << "TEvEtcdKV::TEvRangeRequest\n";
         if (TablesCreating != 0) {
             Requests[Cookie++] = ev;
             if (!InProgress) {
@@ -109,28 +100,22 @@ private:
     }
 
     void Handle(TEvEtcdKV::TEvRangeResponse::TPtr& ev) {
-        std::cerr << "TEvEtcdKV::TEvRangeResponse\n";
-
         auto it = Requests.find(ev->Cookie);
         if (it == Requests.end()) {
-            // TODO [pavelbezpravel]: log error!
-            std::cerr << "Request doesn't exist (TEvEtcdKV::TEvRangeResponse).\n";
+            LOG_E("Request doesn't exist (TEvEtcdKV::TEvRangeResponse).");
             return;
         }
         auto requestVariant = it->second;
         Requests.erase(it);
         const auto* requestPtr = std::get_if<TEvEtcdKV::TEvRangeRequest::TPtr>(&requestVariant);
         if (!requestPtr) {
-            // TODO [pavelbezpravel]: log error!
-            std::cerr << "Request differs from the TEvEtcdKV::TEvRangeRequest type.\n";
+            LOG_E("Request differs from the TEvEtcdKV::TEvRangeRequest type.");
             return;
         }
-
         Send(requestPtr->Get()->Sender, ev->Release());
     }
 
     void Handle(TEvEtcdKV::TEvPutRequest::TPtr& ev) {
-        std::cerr << "TEvEtcdKV::TEvPutRequest\n";
         if (TablesCreating != 0) {
             Requests[Cookie++] = ev;
             if (!InProgress) {
@@ -144,28 +129,22 @@ private:
     }
 
     void Handle(TEvEtcdKV::TEvPutResponse::TPtr& ev) {
-        std::cerr << "TEvEtcdKV::TEvPutResponse\n";
-
         auto it = Requests.find(ev->Cookie);
         if (it == Requests.end()) {
-            // TODO [pavelbezpravel]: log error!
-            std::cerr << "Request doesn't exist (TEvEtcdKV::TEvPutResponse).\n";
+            LOG_E("Request doesn't exist (TEvEtcdKV::TEvPutResponse).");
             return;
         }
         auto requestVariant = it->second;
         Requests.erase(it);
         const auto* requestPtr = std::get_if<TEvEtcdKV::TEvPutRequest::TPtr>(&requestVariant);
         if (!requestPtr) {
-            // TODO [pavelbezpravel]: log error!
-            std::cerr << "Request differs from the TEvEtcdKV::TEvPutRequest type.\n";
+            LOG_E("Request differs from the TEvEtcdKV::TEvPutRequest type.");
             return;
         }
-
         Send(requestPtr->Get()->Sender, ev->Release());
     }
 
     void Handle(TEvEtcdKV::TEvDeleteRangeRequest::TPtr& ev) {
-        std::cerr << "TEvEtcdKV::TEvDeleteRangeRequest\n";
         if (TablesCreating != 0) {
             Requests[Cookie++] = ev;
             if (!InProgress) {
@@ -179,28 +158,22 @@ private:
     }
 
     void Handle(TEvEtcdKV::TEvDeleteRangeResponse::TPtr& ev) {
-        std::cerr << "TEvEtcdKV::TEvDeleteRangeResponse\n";
-
         auto it = Requests.find(ev->Cookie);
         if (it == Requests.end()) {
-            // TODO [pavelbezpravel]: log error!
-            std::cerr << "Request doesn't exist (TEvEtcdKV::TEvDeleteRangeResponse).\n";
+            LOG_E("Request doesn't exist (TEvEtcdKV::TEvDeleteRangeResponse).");
             return;
         }
         auto requestVariant = it->second;
         Requests.erase(it);
         const auto* requestPtr = std::get_if<TEvEtcdKV::TEvDeleteRangeRequest::TPtr>(&requestVariant);
         if (!requestPtr) {
-            // TODO [pavelbezpravel]: log error!
-            std::cerr << "Request differs from the TEvEtcdKV::TEvDeleteRangeRequest type.\n";
+            LOG_E("Request differs from the TEvEtcdKV::TEvDeleteRangeRequest type.");
             return;
         }
-
         Send(requestPtr->Get()->Sender, ev->Release());
     }
 
     void Handle(TEvEtcdKV::TEvTxnRequest::TPtr& ev) {
-        std::cerr << "TEvEtcdKV::TEvTxnRequest\n";
         if (TablesCreating != 0) {
             Requests[Cookie++] = ev;
             if (!InProgress) {
@@ -214,28 +187,22 @@ private:
     }
 
     void Handle(TEvEtcdKV::TEvTxnResponse::TPtr& ev) {
-        std::cerr << "TEvEtcdKV::TEvTxnResponse\n";
-
         auto it = Requests.find(ev->Cookie);
         if (it == Requests.end()) {
-            // TODO [pavelbezpravel]: log error!
-            std::cerr << "Request doesn't exist (TEvEtcdKV::TEvTxnResponse).\n";
+            LOG_E("Request doesn't exist (TEvEtcdKV::TEvTxnResponse).");
             return;
         }
         auto requestVariant = it->second;
         Requests.erase(it);
         const auto* requestPtr = std::get_if<TEvEtcdKV::TEvTxnRequest::TPtr>(&requestVariant);
         if (!requestPtr) {
-            // TODO [pavelbezpravel]: log error!
-            std::cerr << "Request differs from the TEvEtcdKV::TEvTxnRequest type.\n";
+            LOG_E("Request differs from the TEvEtcdKV::TEvTxnRequest type.");
             return;
         }
-
         Send(requestPtr->Get()->Sender, ev->Release());
     }
 
     void Handle(TEvEtcdKV::TEvCompactionRequest::TPtr& ev) {
-        std::cerr << "TEvEtcdKV::TEvCompactionRequest\n";
         if (TablesCreating != 0) {
             Requests[Cookie++] = ev;
             if (!InProgress) {
@@ -249,23 +216,18 @@ private:
     }
 
     void Handle(TEvEtcdKV::TEvCompactionResponse::TPtr& ev) {
-        std::cerr << "TEvEtcdKV::TEvCompactionResponse\n";
-
        auto it = Requests.find(ev->Cookie);
         if (it == Requests.end()) {
-            // TODO [pavelbezpravel]: log error!
-            std::cerr << "Request doesn't exist (TEvEtcdKV::TEvCompactionResponse).\n";
+            LOG_E("Request doesn't exist (TEvEtcdKV::TEvCompactionResponse).");
             return;
         }
         auto requestVariant = it->second;
         Requests.erase(it);
         const auto* requestPtr = std::get_if<TEvEtcdKV::TEvCompactionRequest::TPtr>(&requestVariant);
         if (!requestPtr) {
-            // TODO [pavelbezpravel]: log error!
-            std::cerr << "Request differs from the TEvEtcdKV::TEvCompactionRequest type.\n";
+            LOG_E("Request differs from the TEvEtcdKV::TEvCompactionRequest type.");
             return;
         }
-
         Send(requestPtr->Get()->Sender, ev->Release());
     }
 
